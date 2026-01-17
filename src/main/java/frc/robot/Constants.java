ee199ae07961da4f72f162f3c3406f05996ca7cd
Cynthia Hu <cynthia@team2714.com>
2026-01-16 20:27:02 -0600

    change sparkmax to sparkflex

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,6 +96,6 @@ public final class Constants {
   }
 
   public static final class NeoMotorConstants {
-    public static final double kFreeSpeedRpm = 5676;
+    public static final double kFreeSpeedRpm = 6780;
   }
 }
