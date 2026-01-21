814013824a93280a63be2f77681b344993e21ace
Cynthia Hu <cynthia@team2714.com>
2026-01-20 19:22:01 -0600

    make basic intake functionality

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,17 +13,21 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Intake;
+
 import java.util.List;
 
 /*
@@ -34,10 +38,14 @@ import java.util.List;
  */
 public class RobotContainer {
   // The robot's subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+
+    private final Intake m_intake = new Intake();
+
+
 
   // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -68,11 +76,11 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
+    m_driverController.start().onTrue(new InstantCommand(() -> m_robotDrive.zeroHeading(), m_robotDrive))
         .whileTrue(new RunCommand(() -> m_robotDrive.setX(), m_robotDrive));
 
-    new JoystickButton(m_driverController, XboxController.Button.kStart.value)
-        .onTrue(new InstantCommand(() -> m_robotDrive.zeroHeading(), m_robotDrive));
+    
+    
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,122 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants;
+import frc.robot.Constants.Intake.PivotSetpoints;
+import frc.robot.Constants.Intake.RollerSetpoints;
+
+public class Intake extends SubsystemBase {
+    private SparkFlex m_pivot = new SparkFlex(Constants.Intake.kPivotCanId, MotorType.kBrushless);
+
+    private AbsoluteEncoder m_pivotEncoder = m_pivot.getAbsoluteEncoder();
+
+    private SparkClosedLoopController m_pivotController = m_pivot.getClosedLoopController();
+
+    private SparkFlex m_roller = new SparkFlex(Constants.Intake.kRollerCanId, MotorType.kBrushless);
+
+    private enum PivotSetpoints {
+        STOW,
+        INTAKE,
+        EXTAKE,
+        SCORE
+    }
+
+    private enum RollerSetpoints {
+        INTAKE,
+        EXTAKE,
+        SCORE,
+        STOP
+    }
+
+    public Intake() {
+        // Constructor code here
+        m_pivot.configure(
+                Configs.Intake.pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_roller.configure(
+                Configs.Intake.rollerConfig,
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+    }
+
+    private void setRollerSpeed(RollerSetpoints setpoint) {
+        double speed;
+        switch (setpoint) {
+            case INTAKE:
+                speed = Constants.Intake.RollerSetpoints.kIntake;
+                break;
+            case EXTAKE:
+                speed = Constants.Intake.RollerSetpoints.kExtake;
+                break;
+            case SCORE:
+                speed = Constants.Intake.RollerSetpoints.kScore;
+                break;
+            case STOP:
+                speed = Constants.Intake.RollerSetpoints.kStop;
+                break;
+            default:
+                return;
+        }
+        m_roller.set(speed);
+    }
+
+    private void setPivot(PivotSetpoints setpoint) {
+        double targetPosition = 0;
+        switch (setpoint) {
+            case STOW:
+                targetPosition = Constants.Intake.PivotSetpoints.kStow;
+                break;
+            case INTAKE:
+                targetPosition = Constants.Intake.PivotSetpoints.kIntake;
+                break;
+            case EXTAKE:
+                targetPosition = Constants.Intake.PivotSetpoints.kExtake;
+                break;
+            case SCORE:
+                targetPosition = Constants.Intake.PivotSetpoints.kScore;
+                break;
+            default:
+                return;
+        }
+        m_pivotController.setSetpoint(targetPosition, ControlType.kPosition);
+    }
+
+    public Command intakeCommand() {
+        return this.run(() -> {
+            setPivot(PivotSetpoints.INTAKE);
+            setRollerSpeed(RollerSetpoints.INTAKE);
+        });
+    }
+
+    public Command extakeCommand() {
+        return this.run(() -> {
+            setRollerSpeed(RollerSetpoints.EXTAKE);
+            setPivot(PivotSetpoints.EXTAKE);
+        });
+    }
+
+    public Command scoreCommand() {
+        return this.run(() -> { 
+            setRollerSpeed(RollerSetpoints.SCORE);
+            setPivot(PivotSetpoints.SCORE);
+        });
+    }
+
+    public Command stowCommand() {
+        return this.run(() -> {
+            setRollerSpeed(RollerSetpoints.STOP);
+            setPivot(PivotSetpoints.STOW);
+        });
+    }
+
+}
