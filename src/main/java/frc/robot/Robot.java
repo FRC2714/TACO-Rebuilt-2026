ec0203af46113e3d81cd761715f743f6bc97da77
Cynthia Hu <cynthia@team2714.com>
2026-01-22 20:08:13 -0600

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkSim;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
