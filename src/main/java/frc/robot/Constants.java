6b0ba783eb49c0721972ffbbbcf638a8e512ed68
Dave Oleksy <dave.oleksy@revrobotics.com>
2026-02-26 17:41:02 -0600

    Remap buttons, reverse conveyor direction, change team #

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -101,7 +101,7 @@ public final class Constants {
   }
 
   public static final class Intake {
-    public static final int kRollerCanId = 20; // change later
+    public static final int kRollerCanId = 4; // change later
     public static final int kPivotCanId = 21;
 
     public static final double kPivotReduction = 1;
@@ -109,8 +109,8 @@ public final class Constants {
     public static final double kP = 0.06;
 
     public static final class RollerSetpoints {
-      public static final double kIntake = 0.6;
-      public static final double kExtake = -0.4;
+      public static final double kIntake = -0.6;
+      public static final double kExtake = 0.4;
       public static final double kStop = 0;
     }
 
